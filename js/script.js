cf346d34a17862bd2d27a8476ff74972aaf3b335
filipemateus2000 <filipemateus2000@hotmail.com>
2023-05-24 00:14:02 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+alert('Ola, Seja Bem-Vindo ao Curso !!!')
+document.getElementById('nome').value = 'Oi'
+
+// -> VARIÁVEIS <-//
+
+//string
+var texto = "Curso de JavaScript"
+
+// number
+var numberInteiro = -7
+var numeroFracionado = 11210.75
+
+// boolean
+var teste = false //false -> falso / true -> verdadeiro
+
+//alert(numeroFracionado) //abrir um dialog
+//document.write(texto)
+
+console.log(numberInteiro)
+
+
+
+
+// -> CONCATENAÇÃO <- //
+
+var nomeCompleto = ('Filipe Mateus Barbosa Costa');
+                                        
+
+console.log(nomeCompleto);
+                                        
+
+
+document.write(<h1>'Filipe seu'  + nomeCompleto + 'é' </h1>)
+
+
+// ->Ponto e Virgula <- //
+
+
+var nome = prompt('Digite o Seu Nome:');
+    var idade = prompt('Digite a sua Idade:');
+    var CPF = prompt('Digite a seu CPF:');
+    var RG = prompt('Digite a seu RG:');
+    var Endereço = prompt('Digite seu Endereço:');
+    var idade2 = 50;
+
+    console.log(idade);
+    console.log(idade2);
+
+    document.write('<h1>Olá ' + nome + ', tudo bem? Estou vendo aqui que voce possui ' + idade + ' anos. </h1>');
+    document.write('<h1>Filipe, ' + 'Seu Numero de CPF é ' + CPF + ' e seu RG é, ' + RG + '</h1>');
+    document.write('<h1>Seu Endereço é? ' + Endereço + ' Correto?</h1>');
+    
+ // -> IF/ELSE - INTRODUÇÃO <- //
+
+                                    
+if (condição) {
+// trecho do código a ser executado //
+} else {
+// trecho do código a ser executado //
+}
+if (condição) {
+// trecho de código a ser executado //
+} else if (condição) {
+// trecho do código a ser executado //
+} else {
+// trecho do código a ser executado //
+                                    }
+
+
+// -> Operadores de Comparação (condicionais) <- //
+
+                                        
